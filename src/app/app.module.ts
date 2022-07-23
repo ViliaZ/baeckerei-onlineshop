a30f69b2a7de21fee0a0810b1f0d6a28a5540b9c
Vilia <vzeisig@gmail.com>
2022-07-23 14:18:13 +0200

    main structure, add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
+import { ShopComponent } from './shop/shop.component';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
+import { LegalComponent } from './legal/legal.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    HeroComponent,
+    ShopComponent,
+    ProductComponent,
+    CartComponent,
+    LegalComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
