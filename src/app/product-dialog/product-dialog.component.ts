4fe4723204abc0f08e02fedefcf3bbe85fc8abbe
Vilia <vzeisig@gmail.com>
2022-07-24 15:31:09 +0200

    add Product Dialog Comp to Add new products

---
+++ src/app/product-dialog/product-dialog.component.ts
@@ -0,0 +1,13 @@
+import { Component, Inject, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-dialog',
+  templateUrl: './product-dialog.component.html',
+  styleUrls: ['./product-dialog.component.scss']
+})
+export class ProductDialogComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+}
